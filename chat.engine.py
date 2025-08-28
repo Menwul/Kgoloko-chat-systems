0e4419969ff99ee15d9bad87c29aac6d7de8d23a
Menwul <mzansinet10@gmail.com>
2025-08-28 11:55:51 +0200

    Add files via upload

--- chat.engine.py
+++ chat.engine.py
@@ -895,4 +895,4 @@ def forbidden(e):
     return render_template('403.html'), 403
   
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=5000)
